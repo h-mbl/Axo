a335b1d0292f687d02d2fa5c1e86dee64759d022
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-09-06 20:39:57 -0400

    traduction et mise en page

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ GROQ_API_KEY = data['GROQ_API_KEY']
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 groq_client = Groq(api_key=GROQ_API_KEY)
 
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
@@ -163,7 +164,7 @@ def translate():
     prompt = f"Tu es un traducteur professionnel. Traduis le passage suivant du livre '{book_info.get('title', 'Titre inconnu')}' de l'auteur {book_info.get('author', 'Auteur inconnu')} de l'anglais vers le francais. Voici le texte à traduire :\n\n{text}\n\nTraduction en anglais :"
 
     try:
-        out()
+        #out()
         # model="llama-3.1-70b-versatile",
         completion = groq_client.chat.completions.create(
             model="llama3-groq-70b-8192-tool-use-preview",
